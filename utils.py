edb7c06e5d31dd9ce12ecb74f9f45071d6987a6b
Patrick Hennessy <phennessy517@gmail.com>
2021-04-09 02:34:39 +0000

    Add standard cog error handler

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
+import traceback
+
 import discord
+from discord.ext import commands
 import yaml
 from dotted_dict import DottedDict
+
+from emojis import error
 
 
 def load_config():
@@ -8,20 +13,16 @@ def load_config():
         config = yaml.load(file)
     return DottedDict(config)
 
-async def connect_db(database):
-    # This should be done using normal SQLAlchemy semantics;
-    # The box this is on has a broken dep chain which prevents using create_engine
-    if not database.is_connected:
-        await database.connect()
 
-def error(message):
-    return discord.Embed(title='<:error:828978696957198357> Error', description=message)
-
-def warning(message):
-    return discord.Embed(title='<:warning:828978696680898601> Warning', description=message)
-
-def info(message):
-    return discord.Embed(title='<:info:828978697003728896> Info', description=message)
-
-def success(message):
-    return discord.Embed(title='<:success:828981169861165066> Success', description=message)
+async def cog_error_handler(self, ctx, error_message):
+    if isinstance(error_message, commands.BadArgument):
+        await ctx.send(f"{error} {error_message}")
+    elif isinstance(error_message, commands.TooManyArguments):
+        await ctx.send(f'{error} Too many arguments.')
+        await ctx.send_help(ctx.command)
+    elif isinstance(error_message, commands.MissingRequiredArgument):
+        await ctx.send(f"{error} Missing required arugments")
+        await ctx.send_help(ctx.command)
+    else:
+        tb = traceback.format_exception(None, error_message.original, error_message.original.__traceback__)
+        await ctx.send(f"{error} Unknown error: `{error_message.__class__.__name__}`\n\n<@65497519504764928> ||```\n{''.join(tb)}```||")
