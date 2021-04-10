20077a4cf828ffcea1771b17fc82fad8b7f6cd57
Patrick Hennessy <phennessy517@gmail.com>
2021-04-10 02:23:21 +0000

    Additional error handling

--- utils.py
+++ utils.py
@@ -42,9 +42,11 @@ async def cog_error_handler(self, ctx, error_message):
     elif isinstance(error_message, commands.MissingRequiredArgument):
         await ctx.send(f"{error} Missing required arugments")
         await ctx.send_help(ctx.command)
+    elif isinstance(error_message, commands.MissingAnyRole):
+        await ctx.send(f"{error} {error_message}")
     else:
         tb = traceback.format_exception(None, error_message.original, error_message.original.__traceback__)
-        await ctx.send(f"{error} Unknown error: `{error_message.__class__.__name__}`\n\n<@65497519504764928> ||```\n{''.join(tb)}```||")
+        await ctx.send(f"{error} <@65497519504764928> Unhandled Exception:\n ```\n{''.join(tb)}```")
 
 def setup_logger(level):
     console_handler = logging.StreamHandler(sys.stdout)
