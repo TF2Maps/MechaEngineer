2eafbd93c0cab7f7db69ec7b3940536e3d098f21
Patrick Hennessy <phennessy517@gmail.com>
2021-04-10 02:22:18 +0000

    Add a imp helper command

--- cogs/misc.py
+++ cogs/misc.py
@@ -22,6 +22,25 @@ class Misc(commands.Cog):
     async def on_ready(self):
         await self.bot.change_presence(activity=discord.Game(name="Team Fortress 2"))
 
+    @commands.command(help=config.code.help)
+    @commands.has_any_role(*config.code.role_names)
+    async def test(self, ctx):
+        pass
+
+    @commands.command(help=config.imp.help)
+    @commands.has_any_role(*config.imp.role_names)
+    async def imp(self, ctx):
+        embed = discord.Embed(
+            description=f"{config.imp.description}\n\u200b"
+        )
+        embed.set_author(name=f"Imp Testing Commands", icon_url=global_config.cogs.search.tf2m_icon)
+
+        for category, text in config.imp.command_list.items():
+            embed.add_field(name=f"{category.capitalize()} Commands", value=f"{text}", inline=False)
+
+        embed.set_footer(text=global_config.bot_footer)
+        await ctx.send(embed=embed)
+
     @commands.command(aliases=config.code.aliases, help=config.code.help)
     @commands.has_any_role(*config.code.role_names)
     async def code(self, ctx):
