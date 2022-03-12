527782debedb22ca9280fbd5fef746b1fe4db6e9
Neil S <44575567+neilstottler@users.noreply.github.com>
2022-03-12 00:04:58 -0500

    added error checking

--- cogs/servers.py
+++ cogs/servers.py
@@ -1,5 +1,5 @@
 # Std Lib Imports
-pass
+import socket
 
 # 3rd Party Imports
 import discord
@@ -22,38 +22,47 @@ class Servers(Cog):
     @has_any_role(*config.usserver.role_names)
     @not_nobot_role()
     async def usserver(self, ctx):
-        server = get_srcds_server_info("us.tf2maps.net")
-        embed = await self.get_server_embed(server, "us.tf2maps.net")
-        await ctx.send(embed=embed)
+        try:
+            server = get_srcds_server_info("us.tf2maps.net")
+            embed = await self.get_server_embed(server, "us.tf2maps.net")
+            await ctx.send(embed=embed)
+        except socket.timeout:
+            await ctx.send('US server is offline. Try again later.')
 
     @command(aliases=config.euserver.aliases, help=config.euserver.help)
     @has_any_role(*config.euserver.role_names)
     @not_nobot_role()
     async def euserver(self, ctx):
-        server = get_srcds_server_info("eu.tf2maps.net")
-        embed = await self.get_server_embed(server, "eu.tf2maps.net")
-        await ctx.send(embed=embed)
+        try:
+            server = get_srcds_server_info("eu.tf2maps.net")
+            embed = await self.get_server_embed(server, "eu.tf2maps.net")
+            await ctx.send(embed=embed)
+        except socket.timeout:
+            await ctx.send('EU Server is offline. Try again later.')
 
     @command(aliases=config.active.aliases, help=config.active.help)
     @has_any_role(*config.active.role_names)
     @not_nobot_role()
     async def active(self, ctx):
         alive = False
+ 
+        try:
+            us_server = get_srcds_server_info("us.tf2maps.net")
+            if us_server.player_count > config.active.player_threshold:
+                embed = await self.get_server_embed(us_server, "us.tf2maps.net")
+                await ctx.send(embed=embed)
+                alive = True
 
-        us_server = get_srcds_server_info("us.tf2maps.net")
-        if us_server.player_count > config.active.player_threshold:
-            embed = await self.get_server_embed(us_server, "us.tf2maps.net")
-            await ctx.send(embed=embed)
-            alive = True
+            eu_server = get_srcds_server_info("eu.tf2maps.net")
+            if eu_server.player_count > config.active.player_threshold:
+                embed = await self.get_server_embed(eu_server, "eu.tf2maps.net")
+                await ctx.send(embed=embed)
+                alive = True
 
-        eu_server = get_srcds_server_info("eu.tf2maps.net")
-        if eu_server.player_count > config.active.player_threshold:
-            embed = await self.get_server_embed(eu_server, "eu.tf2maps.net")
-            await ctx.send(embed=embed)
-            alive = True
-
-        if not alive:
-            await ctx.send(f"{warning} No map tests are currently happening right now. Check back later")
+            if not alive:
+                await ctx.send(f"{warning} No map tests are currently happening right now. Check back later")
+        except socket.timeout:
+            await ctx.send('A server is offline. Please use !eu or !us for the time being.')
 
     @staticmethod
     async def get_server_embed(server_data, host):
