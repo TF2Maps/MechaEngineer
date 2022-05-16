e17bdcd0be138e10050b35aad1d1016ff6437690
Neil S <44575567+neilstottler@users.noreply.github.com>
2022-05-16 13:02:58 -0400

    added support for update command
    made it so you don't have to type the map name twice anymore in order to update a map

--- cogs/maplist.py
+++ cogs/maplist.py
@@ -18,14 +18,6 @@ import discord
 import httpx
 from tortoise.expressions import Q
 
-#Slash Command Imports
-from discord.commands import (
-    slash_command,
-    Option,
-    message_command,
-    user_command
-)
-
 # Local Imports
 from utils import load_config, cog_error_handler, get_srcds_server_info
 from utils.emojis import success, warning, error, info, loading
@@ -42,14 +34,16 @@ config = global_config.cogs.maplist
 class MapList(Cog):
     cog_command_error = cog_error_handler
 
-    @discord.slash_command(description="Check the upload status of your map.")
-    async def uploadcheck(self, ctx, file_name: discord.Option(str)):
-        message = await ctx.respond(f"{loading} Checking...")
+    @command()
+    @has_any_role(*config.add.role_names)
+    @not_nobot_role()
+    async def uploadcheck(self, ctx, map_name):
+        message = await ctx.reply(f"{loading} Checking...")
 
         us, eu, redirect = await asyncio.gather(
-            remote_file_exists(f"{file_name}.bsp", **global_config.sftp.us_tf2maps_net),
-            remote_file_exists(f"{file_name}.bsp", **global_config.sftp.eu_tf2maps_net),
-            redirect_file_exists(f"{file_name}.bsp.bz2", global_config['vultr_s3_client']),
+            remote_file_exists(f"{map_name}.bsp", **global_config.sftp.us_tf2maps_net),
+            remote_file_exists(f"{map_name}.bsp", **global_config.sftp.eu_tf2maps_net),
+            redirect_file_exists(f"{map_name}.bsp.bz2", global_config['vultr_s3_client']),
         )
 
         output = ""
@@ -70,31 +64,37 @@ class MapList(Cog):
         embed.set_author(name=f"Map Upload Status")
         embed.set_footer(text=global_config.bot_footer)
 
-        await ctx.edit(embed=embed, content="")
+        await message.edit(embed=embed, content="")
 
-    @discord.slash_command(description="Add your map to the map testing queue.")
-    async def add(self, ctx, link: discord.Option(str), *, notes: discord.Option(str, required=False)):
-        message = await ctx.respond(f"{loading} Adding your map...")
+    @command(aliases=config.add.aliases, help=config.add.help)
+    @has_any_role(*config.add.role_names)
+    @not_nobot_role()
+    async def add(self, ctx, link, *, notes=""):
+        message = await ctx.reply(f"{loading} Adding your map...")
         await self.add_map(ctx, message, link, notes)
 
-    @discord.slash_command(description="Update your map while keeping its slot in the queue.")
+    @command(aliases=config.update.aliases, help=config.update.help)
+    @has_any_role(*config.update.role_names)
+    @not_nobot_role()
     async def update(self, ctx, map_name, link, *, notes=""):
         maps = await Maps.filter(map__icontains=map_name, status="pending", discord_user_id=ctx.author.id).all()
 
         if len(maps) == 0:
-            await ctx.respond(content=f"{error} You don't have a map with that name on the list!")
+            await ctx.send(f"{error} You don't have a map with that name on the list!")
         else:
             if link == "-":
                 if not notes:
-                    await ctx.respond(content=f"{error} Add a link or notes, otherwise theres nothing to update.")
+                    await ctx.reply(f"{error} Add a link or notes, otherwise theres nothing to update.")
                 maps[0].notes = notes
                 await maps[0].save()
-                await ctx.respond(content=f"{success} Updated the notes for `{maps[0].map}`!")
+                await ctx.reply(f"{success} Updated the notes for `{maps[0].map}`!")
             else:
-                message = await ctx.respond(content=f"{loading} Updating your map...")
+                message = await ctx.reply(f"{loading} Updating your map...")
                 await self.add_map(ctx, message, link, notes, old_map=maps[0])
 
-    @discord.slash_command(description="Remove your map from the map testing queue.")
+    @command(aliases=config.delete.aliases, help=config.delete.help)
+    @has_any_role(*config.delete.role_names)
+    @not_nobot_role()
     async def delete(self, ctx, map_name):
         maps = []
         override_roles = set(config.delete.override_roles)
@@ -108,12 +108,15 @@ class MapList(Cog):
             maps = await Maps.filter(map__icontains=map_name, status="pending", discord_user_id=ctx.author.id).all()
 
         if len(maps) == 0:
-            await ctx.respond(f"{error} You don't have a map with that name on the list!")
+            await ctx.send(f"{error} You don't have a map with that name on the list!")
         else:
             await maps[0].delete()
-            await ctx.respond(f"{success} Deleted `{maps[0].map}` from the list.")
+            await ctx.send(f"{success} Deleted `{maps[0].map}` from the list.")
 
-    @discord.slash_command(description="See the current map testing queue.")
+
+    @command(aliases=config.maps.aliases, help=config.maps.help)
+    @has_any_role(*config.maps.role_names)
+    @not_nobot_role()
     async def maps(self, ctx):
         # us_server = get_srcds_server_info("us.tf2maps.net")
         # eu_server = get_srcds_server_info("eu.tf2maps.net")
@@ -140,7 +143,7 @@ class MapList(Cog):
         embed.add_field(name="Map Queue", value=map_names, inline=False)
         embed.set_footer(text=global_config.bot_footer)
 
-        await ctx.respond(embed=embed)
+        await ctx.send(embed=embed)
 
     async def add_map(self, ctx, message, link, notes="", old_map=None):
         # If not link; use fuzzy search
@@ -148,28 +151,27 @@ class MapList(Cog):
             try:
                 link = await search_downloads(link, discord_user_id=ctx.author.id)
             except ForumUserNotFoundException:
-                await ctx.edit(content=f"{error} You either need to provide a link or need to connect your TF2Maps.net account to Discord. See <#{global_config.faq_channel_id}>")
+                await message.edit(content=f"{error} You either need to provide a link or need to connect your TF2Maps.net account to Discord. See <#{global_config.faq_channel_id}>")
                 return
-                 
 
             if not link:
-                await ctx.edit(content=f"{error} Could not find a download by the name. Try using a link instead.")
+                await message.edit(content=f"{error} Could not find a download by the name. Try using a link instead.")
+                await ctx.send_help(ctx.command)
                 return
-
             else:
                 if len(link) > 1:
-                    await ctx.edit(content=f"{error} Found multiple links. Use a more specific link.")
+                    await message.edit(content=f"{error} Found multiple links. Use a more specific link.")
                     return
-
                 link = link[0]
 
         # Find map download in link
         link = await self.parse_link(link)
         if not link:
-            await ctx.edit(content=f"{error} No valid link found.")
+            await message.edit(content=f"{error} No valid link found.")
+            await ctx.send_help(ctx.command)
             return
 
-        await ctx.edit(content=f"{loading} Found link: {link}")
+        await message.edit(content=f"{loading} Found link: {link}")
 
         # Get map info
         filename = await get_download_filename(link)
@@ -178,30 +180,31 @@ class MapList(Cog):
         
         # Must be a BSP
         if not re.search("\.bsp$", filename):
-            await ctx.edit(content=f"{warning} `{map_name}` is not a BSP!")
+            await message.edit(content=f"{warning} `{map_name}` is not a BSP!")
             return
 
         # Check for dupe
         already_in_queue = await Maps.filter(map=map_name, status="pending").all()
         if len(already_in_queue) > 0 and not old_map:
-            await ctx.edit(content=f"{warning} `{map_name}` is already on the list!")
+            await message.edit(content=f"{warning} `{map_name}` is already on the list!")
             return
 
         # Download the map
-        await ctx.edit(content=f"{loading} Found file name: `{filename}`. Downloading...")
+        await message.edit(content=f"{loading} Found file name: `{filename}`. Downloading...")
         await download_file(link, filepath)
         
         # Compress file for redirect
-        await ctx.edit(content=f"{loading} Compressing `{filename}` for faster downloads...")
+        await message.edit(content=f"{loading} Compressing `{filename}` for faster downloads...")
         compressed_file = compress_file(filepath)
 
         # Ensure map has the same MD5 sum as an existing one
         if await redirect_file_exists(compressed_file, global_config['vultr_s3_client']):
             if not await check_redirect_hash(compressed_file, global_config['vultr_s3_client']):
-                await ctx.edit(content=f"{warning} Your map `{map_name}` differs from the map on the server. Please upload a new version of the map.")
+                await message.edit(content=f"{warning} Your map `{map_name}` differs from the map on the server. Please upload a new version of the map.")
                 return
 
         # Upload to servers
+        await message.edit(content=f"{loading} Uploading `{filename}` to servers...")
         await asyncio.gather(
             upload_to_gameserver(filepath, **global_config.sftp.us_tf2maps_net),
             upload_to_gameserver(filepath, **global_config.sftp.eu_tf2maps_net),
@@ -209,14 +212,15 @@ class MapList(Cog):
         )
 
         # Insert map into DB
+        await message.edit(content=f"{loading} Putting `{map_name}` into the map queue...")
+
         if old_map:
             old_map.url = link
             old_map.map = map_name
             if notes:
                 old_map.notes = notes
             await old_map.save()
-            await ctx.edit(content=f"{success} Updated `{map_name}` successfully! Ready for testing!")
-
+            await message.edit(content=f"{success} Updated `{map_name}` successfully! Ready for testing!")
         else:
             await Maps.create(
                 discord_user_handle=f"{ctx.author.name}#{ctx.author.discriminator}",
@@ -227,7 +231,8 @@ class MapList(Cog):
                 notes=notes,
                 added=datetime.now()
             )
-            await ctx.edit(content=f"{success} Uploaded `{map_name}` successfully! Ready for testing!")
+            await message.edit(content=f"{success} Uploaded `{map_name}` successfully! Ready for testing!")
+
 
     @staticmethod
     async def parse_link(link):
