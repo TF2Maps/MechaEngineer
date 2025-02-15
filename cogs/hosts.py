58958970099b0b6471fe0a5fb1c0b58f3c77fd80
Neil S <44575567+neilstottler@users.noreply.github.com>
2025-02-15 14:29:00 -0500

    refactored hosts

--- cogs/hosts.py
+++ cogs/hosts.py
@@ -10,8 +10,6 @@ import ast
 import discord
 from discord.ext.commands import Cog, slash_command
 import databases
-from rcon.source import rcon
-from steam.steamid import SteamID
 
 # Local Imports
 from utils import load_config, cog_error_handler
@@ -101,7 +99,6 @@ class Hosts(Cog):
 
         user_id = result[0]
         user_secondary_groups = await self.get_user_roles(user_id)
-        print(user_secondary_groups)
 
         if 41 not in user_secondary_groups:
             await ctx.edit(content=f"{warning} {user.name} is already removed from the xenforo group! Checking discord...")
@@ -133,104 +130,82 @@ class Hosts(Cog):
             not_nobot_role_slash()
         ]
     )
-    async def temphost(self, ctx):
-
-        #check if vip first
-        vip_role = discord.utils.get(ctx.guild.roles, name="VIP")
-
-        if vip_role not in ctx.author.roles:
-            await ctx.respond(f"{warning} You're not a vip, only vip's may get temp-hosting powers for gamedays.")
-            return
-        
+    async def temphost(self, ctx, user: discord.Option(discord.Member, description="discord user")):
+     
         #check if host already
         hosts_role = discord.utils.get(ctx.guild.roles, name="Hosts")
 
         if hosts_role in ctx.author.roles:
-            await ctx.respond(f"{warning} You're a host already. Go away.")
+            await ctx.respond(f"{warning} They're a host already. Go away.")
+            return
+        
+        #check if temp-host already
+        temphosts_role = discord.utils.get(ctx.guild.roles, name="Temp-Host")
+
+        if temphosts_role in ctx.author.roles:
+            await ctx.respond(f"{warning} They're a temp-host already. Go away.")
             return
 
-
-        #
-        # check if user has forum account
-        #
+        #do site things
+        await ctx.respond(f"{loading} Adding {user.name} to hosts group...")
+        time.sleep(2)
         database = databases.Database(global_config.databases.tf2maps_site)
         await database.connect()
         
         #get their forum ID
         query = "SELECT user_id FROM xf_user_connected_account WHERE provider_key = :field_value"
-        values = {"field_value": ctx.author.id}
+        values = {"field_value": user.id}
         result = await database.fetch_one(query=query, values=values)
 
         #has connected discord account check
         if not result:
-            await ctx.edit(content=f"{error} You don't have a Discord User ID # set in your TF2Maps.net profile.\nSee [this help page](<http://bot.tf2maps.net/faq>) on how to get started.")
-            return
-        
-        user_forum_id = result[0]
-
-        #check if server has a lot of people on it
-            #how to know what server they are on
-                #rcon
-            #is that worth the effort?
-
-        query = "SELECT provider_key FROM xf_user_connected_account WHERE provider = 'th_cap_steam' AND user_id = :user_id"
-        values = {"user_id": user_forum_id}
-        result = await database.fetch_one(query=query, values=values)
-        if result is None:
-            await ctx.respond(f"{error} You do not have a steam account linked.")
-            return
-        user_steam_id = result[0].decode("utf-8")
-
-        user_steam3 = SteamID(user_steam_id).as_steam3
-
-        on_server = False
-
-        response_us = await rcon('api_steamids', host="us.tf2maps.net", port=27015, passwd=sftp_config.us_tf2maps_net.rcon)
-        players_us = ast.literal_eval(response_us)
-
-        #loop through api for player steamid
-        for player in players_us:
-            steam_id = player['steam_id']
-            if steam_id == user_steam3:
-                if len(players_us) > 16:
-                    await ctx.respond(f"{warning} There is more than 16 players on the US server. We are assuming there is a map test on-going.")
-                    return
-                on_server = True
-                break
-
-        response_eu = await rcon('api_steamids', host="eu.tf2maps.net", port=27015, passwd=sftp_config.eu_tf2maps_net.rcon)
-        players_eu = ast.literal_eval(response_eu)
-
-        #loop through api for player steamid
-        for player in players_eu:
-            steam_id = player['steam_id']
-            if steam_id == user_steam3:
-                if len(players_us) > 16:
-                    await ctx.respond(f"{warning} There is more than 16 players on the EU server. We are assuming there is a map test on-going.")
-                    return
-                on_server = True
-                break
-
-        if on_server is False:
-            await ctx.respond(f"{error} You are not present on the server or you haven't linked your accounts to the forums.")
+            await ctx.edit(content=f"{error} {user.name} doesn't have a Discord User ID # set in their TF2Maps.net profile.\nSee [this help page](<http://bot.tf2maps.net/faq.php>) on how to get started.")
             return
 
-        #grant temp host
-        await ctx.respond(f"{loading} Adding you to the temp-hosts forum group...")
-        time.sleep(2)
-
-        #add to role
-        user_secondary_groups = await self.get_user_roles(user_forum_id)
+        user_id = result[0]
+        user_secondary_groups = await self.get_user_roles(user_id)
 
         if 43 in user_secondary_groups:
-            await ctx.edit(content=f"{warning} {ctx.author.name} already has the temp-hosts group on the forum!")
+            await ctx.edit(content=f"{warning} {user.name} already has the temp-host group on the forum! Checking discord...")
             time.sleep(3)
         else:
-            message = await ctx.edit(content=f"{loading} Adding {ctx.author.name} to temp-hosts group on the forums...")
+            message = await ctx.edit(content=f"{loading} Adding {user.name} to temp-host group on the forums...")
             time.sleep(2)
-            await self.add_user_temp_host(ctx, message, user_forum_id, user_secondary_groups, ctx.author)
+            await self.add_user_hosts(ctx, message, user_id, user_secondary_groups, user)
+        
 
-        pass
+        message = await ctx.edit(content=f"{loading} Adding {user.name} to hosts group on discord...")
+        time.sleep(2)
+        ##discord role stuff
+        temphost_role = discord.utils.get(ctx.guild.roles, name="Temp-Host")
+        if temphost_role in ctx.user.roles:
+            await ctx.edit(content=f"{warning} They're already a Host, go away.")
+            return
+        await user.add_roles(temphosts_role)
+        await ctx.edit(content=f"{success} Added {user.name} to the Temp-Host group on discord.")
+        
+    @slash_command(
+        name="listtemphosts", 
+        description=config.listtemphosts.help, 
+        guild_ids=global_config.bot_guild_ids,
+        checks=[
+            roles_required(config.listtemphosts.role_names),
+            not_nobot_role_slash()
+        ]
+    )
+    async def listtemphosts(self, ctx):
+
+        temphost_role = discord.utils.get(ctx.guild.roles, name="Temp-Host")
+        if len(temphost_role.members) == 0:
+            await ctx.respond("No one has the Temp-Host role.")
+            return
+
+        output_list = ""
+        for member in temphost_role.members:
+            output_list = output_list + str(member.name) + "\n"
+        await ctx.respond("Here is the list of users who have the temp-host role: ```" + output_list + "```If there are no gamedays, no one should have this role.")
+        return
+
 
     async def get_user_roles(self, user_id):
         #
